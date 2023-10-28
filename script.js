8bb906c937f5bade9e8f12559313a325d00220c8
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-28 16:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
-//your code here
-// An array of band names
-let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Led Zeppelin', 'Nirvana'];
+let bandNames = ['The Beatles', 'The Who', 'Aerosmith', 'Led Zeppelin', 'Anthrax'];
+let sortedBandNames = bandNames.sort((a, b) => {
+  let aName = a.replace(/^(a|an|the)\s+/i, '');
+  let bName = b.replace(/^(a|an|the)\s+/i, '');
+  return aName.localeCompare(bName);
+});
 
-// Function to remove articles and sort the band names
-function sortBandNamesWithoutArticles(bandNames) {
-  const articles = ['a', 'an', 'the'];
-  
-  // Custom sorting function
-  const customSort = (a, b) => {
-    a = a.toLowerCase();
-    b = b.toLowerCase();
+let ul = document.createElement('ul');
+ul.id = 'band';
+document.body.appendChild(ul);
 
-    for (const article of articles) {
-      if (a.startsWith(article + ' ')) {
-        a = a.slice(article.length + 1);
-        break;
-      }
-    }
-
-    for (const article of articles) {
-      if (b.startsWith(article + ' ')) {
-        b = b.slice(article.length + 1);
-        break;
-      }
-    }
-
-    return a.localeCompare(b);
-  };
-
-  // Sort the band names using the custom sorting function
-  bandNames.sort(customSort);
-
-  return bandNames;
-}
-
-// Sort the band names without articles
-const sortedBandNames = sortBandNamesWithoutArticles(bandNames);
-
-// Update the HTML content dynamically
-const bandList = document.getElementById('band');
-bandList.innerHTML = sortedBandNames.map(band => `<li>${band}</li>`).join('');
-
+sortedBandNames.forEach((name) => {
+  let li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
